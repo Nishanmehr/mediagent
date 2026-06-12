ebd2a80ae058a03cdfaa4051bffaa2796f24e681
nishant mehra <nishantmehra172@gmail.com>
2026-06-13 02:22:08 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/ai/mediagent/MediagentApplication.java
@@ -0,0 +1,13 @@
+package com.ai.mediagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediagentApplication.class, args);
+	}
+
+}
